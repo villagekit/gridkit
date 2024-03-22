b71f6fa7bea3a5af9ccbc12be02fdc8d14cd3b88
Michael Williams <michael.williams@enspiral.com>
2024-03-22 15:44:05 +1300

    do dah doo

--- products/kit/scripts/generate-comlink-dataurl.cjs
+++ products/kit/scripts/generate-comlink-dataurl.cjs
@@ -5,6 +5,8 @@ const comlinkSrcPath = require.resolve('comlink/dist/esm/comlink.mjs')
 const comlinkData = Buffer.from(readFileSync(comlinkSrcPath, 'utf8'))
 const comlinkDataBase64 = comlinkData.toString('base64')
 const comlinkDataUrl = `data:application/javascript;base64,${comlinkDataBase64}`
-const comlinkDataUrlFile = `export const comlinkDataUrl = "${comlinkDataUrl}"`
+const comlinkDataUrlFile = `export const comlinkDataUrl =
+  '${comlinkDataUrl}'
+`
 const comlinkDstPath = join(__dirname, '../src/comlink.ts')
 writeFileSync(comlinkDstPath, comlinkDataUrlFile, 'utf8')
