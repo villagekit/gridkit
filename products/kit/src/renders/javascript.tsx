b7d308fc863b06d7f315cc6ef7c05663b370aa8c
Michael Williams <hello@mikey.nz>
2024-07-31 11:00:39 +1200

    asyncify

--- products/kit/src/renders/javascript.tsx
+++ products/kit/src/renders/javascript.tsx
@@ -18,27 +18,35 @@ type Evaluator = {
 
 export const javascriptRenderer = fromCallback<RenderEvent, RendererMachineEvent>(
   ({ sendBack, receive }) => {
-    const evaluatorIframe = createEvaulatorIframe()
-    document.body.appendChild(evaluatorIframe)
+    const loadEvaluator: Promise<[Comlink.Remote<Evaluator>, HTMLIFrameElement]> = new Promise(
+      (resolve) => {
+        ;(async () => {
+          const evaluatorIframe = await createEvaulatorIframe()
+          document.body.appendChild(evaluatorIframe)
 
-    const evaluator = Comlink.wrap<Evaluator>(
-      Comlink.windowEndpoint(evaluatorIframe.contentWindow!),
+          const evaluator = Comlink.wrap<Evaluator>(
+            Comlink.windowEndpoint(evaluatorIframe.contentWindow!),
+          )
+          evaluatorIframe.onload = () => resolve([evaluator, evaluatorIframe])
+        })()
+      },
     )
-    const hasLoadedEvaluator = new Promise((resolve) => {
-      evaluatorIframe.onload = resolve
-    })
 
     receive((event) => {
       handleCode(event.code)
     })
 
     return () => {
-      evaluator[Comlink.releaseProxy]()
-      document.body.removeChild(evaluatorIframe)
+      ;(async () => {
+        const [evaluator, evaluatorIframe] = await loadEvaluator
+
+        evaluator![Comlink.releaseProxy]()
+        document.body.removeChild(evaluatorIframe!)
+      })()
     }
 
     async function handleCode(jsCode: string) {
-      await hasLoadedEvaluator
+      const [evaluator] = await loadEvaluator
 
       const traceMap = getTraceMap(jsCode)
 
@@ -105,65 +113,59 @@ export const javascriptRenderer = fromCallback<RenderEvent, RendererMachineEvent
   },
 )
 
-const createEvaulatorIframe = () => {
+const createEvaulatorIframe = async () => {
   const iframe = document.createElement('iframe')
   iframe.title = 'Village Kit Evaluator'
   iframe.sandbox.add('allow-scripts')
   iframe.sandbox.add('allow-same-origin')
   iframe.style.display = 'none'
-  iframe.srcdoc = createEvaluatorIframeSrc()
+  iframe.srcdoc = await createEvaluatorIframeSrc()
   return iframe
 }
 
-const newModule = (code: string) =>
+const loadImport = (code: string) =>
   URL.createObjectURL(new Blob([code], { type: 'text/javascript' }))
 
-// @ts-ignore
-const partCreatorUrl = newModule(await import('../../../../core/part/dist/creator.js?raw').default)
-const comlinkUrl = newModule(
+const loadImports = Promise.all([
   // @ts-ignore
-  await import('../../../../node_modules/comlink/dist/esm/comlink.js?raw').default,
-)
-const threeUrl = newModule(
+  import('../../../../node_modules/comlink/dist/esm/comlink.js?raw'),
   // @ts-ignore
-  await import('../../../../node_modules/three/build/three.module.js?raw').default,
-)
-const fastenerCreatorUrl = newModule(
+  import('../../../../node_modules/three/build/three.module.js?raw'),
   // @ts-ignore
-  await import('../../../../parts/fastener/dist-bundles/creator.js?raw').default,
-)
-const gridbeamCreatorUrl = newModule(
+  import('../../../../util/math/dist/index.js?raw'),
   // @ts-ignore
-  await import('../../../../parts/gridbeam/dist-bundles/creator.js?raw').default,
-)
-const gridpanelCreatorUrl = newModule(
+  import('../../../../util/units/dist/index.js?raw'),
   // @ts-ignore
-  await import('../../../../parts/gridpanel/dist-bundles/creator.js?raw').default,
-)
-// @ts-ignore
-const mathUrl = newModule(await import('../../../../util/math/dist/index.js?raw').default)
-// @ts-ignore
-const unitsUrl = newModule(await import('../../../../util/units/dist/index.js?raw').default)
+  import('../../../../core/part/dist/creator.js?raw'),
+  // @ts-ignore
+  import('../../../../parts/gridbeam/dist-bundles/creator.js?raw'),
+  // @ts-ignore
+  import('../../../../parts/gridpanel/dist-bundles/creator.js?raw'),
+  // @ts-ignore
+  import('../../../../parts/fastener/dist-bundles/creator.js?raw'),
+]).then((modules) => {
+  const loaded = modules.map((module: any) => module.default).map(loadImport)
+  const [comlink, three, math, units, partBase, partGridbeam, partGridpanel, partFastener] = loaded
+  return {
+    comlink: comlink,
+    three: three,
+    '@villagekit/math': math,
+    '@villagekit/units': units,
+    '@villagekit/part/creator': partBase,
+    '@villagekit/part-gridbeam/creator': partGridbeam,
+    '@villagekit/part-gridpanel/creator': partGridpanel,
+    '@villagekit/part-fastener/creator': partFastener,
+    '@villagekit/design/kit': loadImport(''),
+  }
+})
 
-console.log('url', comlinkUrl)
+const createEvaluatorIframeSrc = async () => {
+  const imports = await loadImports
 
-const createEvaluatorIframeSrc = () =>
-  `
+  return `
 <!doctype html>
 <script type="importmap">
-{
-  "imports": {
-    "comlink": "${comlinkUrl}",
-    "three": "${threeUrl}",
-    "@villagekit/math": "${mathUrl}",
-    "@villagekit/units": "${unitsUrl}",
-    "@villagekit/part/creator": "${partCreatorUrl}",
-    "@villagekit/part-gridbeam/creator": "${gridbeamCreatorUrl}",
-    "@villagekit/part-gridpanel/creator": "${gridpanelCreatorUrl}",
-    "@villagekit/part-fastener/creator": "${fastenerCreatorUrl}",
-    "@villagekit/design/kit": "data:,${encodeURI('')}"
-  }
-}
+${JSON.stringify({ imports })}
 </script>
 <script type="module">
   import * as Comlink from "comlink"
@@ -209,85 +211,7 @@ const createEvaluatorIframeSrc = () =>
 </script>
 </html>
 `
-
-/*
-const createEvaulatorWorkerSrc = () => `
-  // web workers don't yet support importmaps
-  import * as Comlink from "${comlinkDataUrl}"
-
-  let moduleUrl = null
-  let module = null
-
-  function loadModule(code) {
-    if (moduleUrl != null) {
-      URL.revokeObjectURL(moduleUrl)
-    }
-
-    moduleUrl = URL.createObjectURL(
-      new Blob([code], { type: 'text/javascript' }),
-    )
-
-    return moduleUrl
-  }
-
-  async function evaluateModule() {
-    module = await import(moduleUrl)
-
-    const { parameters, presets, parts, plugins } = module
-
-    if (typeof parts === 'function') {
-      return { parameters, presets, plugins }
-    } else {
-      return { parts, plugins }
-    }
-  }
-
-  function evaluateParts(parameters, partVariants) {
-    return module.parts(parameters, partVariants)
-  }
-
-  const exports = {
-    loadModule,
-    evaluateModule,
-    evaluateParts,
-  }
-
-  Comlink.expose(exports)
-`
-
-const createEvaluatorIframeSrc = () =>
-  `
-<!doctype html>
-<script type="importmap">
-{
-  "imports": {
-    "comlink": "${comlinkDataUrl}",
-    "three": "https://cdn.jsdelivr.net/npm/three@0.165.0/+esm",
-    "react": "https://cdn.jsdelivr.net/npm/react@18.3.1/+esm",
-    "@react-three/fiber": "https://cdn.jsdelivr.net/npm/react-three-fiber@6.0.13/+esm",
-    "@villagekit/math": "data:application/javascript;base64,${encodeURI(encodeBase64(mathRaw))}",
-    "@villagekit/units": "data:application/javascript;base64,${encodeURI(encodeBase64(unitsRaw))}",
-    "@villagekit/part/creator": "data:application/javascript;base64,${encodeURI(encodeBase64(partCreatorRaw))}",
-    "@villagekit/part-gridbeam/creator": "data:application/javascript;base64,${encodeURI(encodeBase64(gridbeamCreatorRaw))}",
-    "@villagekit/design/kit": "data:,${encodeURI('')}"
-  }
 }
-</script>
-<script type="module">
-  import * as Comlink from "comlink"
-
-  const workerCode = \`${createEvaulatorWorkerSrc()}\`
-  const workerUrl = URL.createObjectURL(
-    new Blob([workerCode], { type: 'text/javascript' })
-  )
-  const workerObj = new Worker(workerUrl, { type: 'module' })
-  const worker = Comlink.wrap(workerObj)
-
-  Comlink.expose(worker, Comlink.windowEndpoint(self.parent))
-</script>
-</html>
-`
-*/
 
 function getTraceMap(code: string) {
   const sourceMapLine = code.substring(code.lastIndexOf('\n', code.length - 1) + 1, code.length)
