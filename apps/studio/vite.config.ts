efe644bf2047ad703e2cd1b68eeb3df0ad618c32
Michael Williams <hello@mikey.nz>
2024-08-29 17:22:09 +1200

    start Tauri

---
+++ apps/studio/vite.config.ts
@@ -0,0 +1,66 @@
+import { readFile, readdir, realpath } from 'node:fs/promises'
+import { join } from 'node:path'
+import react from '@vitejs/plugin-react'
+import { reverse, sortBy } from 'lodash-es'
+import { defineConfig } from 'vite'
+
+const host = process.env.TAURI_DEV_HOST
+
+// https://vitejs.dev/config/
+export default defineConfig(async () => ({
+  plugins: [react()],
+
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  //
+  // 1. prevent vite from obscuring rust errors
+  clearScreen: false,
+  // 2. tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 1420,
+    strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: 'ws',
+          host,
+          port: 1421,
+        }
+      : undefined,
+    watch: {
+      // 3. tell vite to ignore watching `src-tauri`
+      ignored: ['**/src-tauri/**'],
+    },
+  },
+  resolve: {
+    mainFields: ['module', 'jsnext:main', 'jsnext'],
+    alias: {
+      ...(await workspaceAliases()),
+    },
+  },
+}))
+
+async function workspaceAliases() {
+  const aliases: Record<string, string> = {}
+  const workspacePkgsDir = join(__dirname, '../../node_modules/@villagekit')
+  const workspacePkgs = await readdir(workspacePkgsDir)
+  await Promise.all(
+    workspacePkgs.map(async (pkgName) => {
+      const pkgBase = await realpath(join(workspacePkgsDir, pkgName))
+      const pkgJson = JSON.parse(await readFile(join(pkgBase, 'package.json'), 'utf8'))
+      type ExportMap = string | { source?: string; import: string }
+      if (pkgJson['exports'] == null) return
+      let exportEntries = Object.entries<ExportMap>(pkgJson['exports'])
+      // NOTE (mw): sort so ./sub comes before ./
+      //   important for module like @villagekit/part/base
+      exportEntries = reverse(sortBy(exportEntries, ['[0]']))
+      for (const [exportKey, exportMap] of exportEntries) {
+        const aliasKey = join('@villagekit', pkgName, exportKey)
+        const aliasTo =
+          typeof exportMap === 'string' ? exportMap : exportMap.source || exportMap.import
+        const aliasValue = join(pkgBase, aliasTo)
+        aliases[aliasKey] = aliasValue
+      }
+    }),
+  )
+  return aliases
+}
