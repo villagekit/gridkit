fc335a804e74c9b5840cc5e108e48edd1693913b
Michael Williams <hello@mikey.nz>
2024-08-30 23:36:29 +1200

    remove old electron app

--- apps/studio-electron/src/client.ts
+++
@@ -1,4 +0,0 @@
-import type { Router } from '@/api'
-import { createTRPCReact } from '@trpc/react-query'
-
-export const client: ReturnType<typeof createTRPCReact<Router>> = createTRPCReact<Router>()
