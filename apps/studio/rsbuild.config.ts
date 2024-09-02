674368bf76007869bd65ceb38fa3fd1c9f5a2f78
Michael Williams <hello@mikey.nz>
2024-09-02 22:46:52 +1200

    do dah doo

--- apps/studio/rsbuild.config.ts
+++ apps/studio/rsbuild.config.ts
@@ -5,7 +5,17 @@ import { pluginSourceBuild } from '@rsbuild/plugin-source-build'
 const host = process.env.TAURI_DEV_HOST
 
 export default defineConfig({
-  plugins: [pluginReact(), pluginSourceBuild()],
+  plugins: [
+    pluginReact({
+      reactRefreshOptions: {
+        exclude: [/.*/, /worker.ts$/],
+      },
+    }),
+    pluginSourceBuild(),
+  ],
+  dev: {
+    hmr: true,
+  },
   html: {
     template: './index.html',
   },
@@ -14,10 +24,33 @@ export default defineConfig({
       index: './src/main.tsx',
     },
   },
-  // tauri expects a fixed port, fail if that port is not available
   server: {
+    // tauri expects a fixed port, fail if that port is not available
     port: 1420,
     strictPort: true,
-    host: host,
+    host,
+  },
+  tools: {
+    rspack: {
+      module: {
+        // support ?raw imports
+        rules: [
+          {
+            resourceQuery: /raw/,
+            type: 'asset/source',
+          },
+        ],
+        // typescript/lib/typescript: https://github.com/microsoft/TypeScript/issues/39436
+        noParse: new RegExp(['node_modules/typescript/lib/typescript.js'].join('|')),
+      },
+      // @typescript/vfs/dist/vfs.esm.js: https://github.com/microsoft/TypeScript-Website/pull/3022
+      //   also have the dep patched with pnpm.
+      resolve: {
+        fallback: {
+          path: false,
+          fs: false,
+        },
+      },
+    },
   },
 })
