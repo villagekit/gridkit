960d41c38626da621338b92adb4866fde79f7960
Michael Williams <hello@mikey.nz>
2024-10-17 14:03:03 +1300

    Upgrade to Tauri 2.0 stable

--- apps/studio/vite.config.ts
+++ apps/studio/vite.config.ts
@@ -10,27 +10,31 @@ const host = process.env.TAURI_DEV_HOST
 export default defineConfig(async () => ({
   plugins: [tsconfigPaths()],
 
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent vite from obscuring rust errors
+  // prevent vite from obscuring rust errors
   clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
   server: {
-    port: 1420,
+    // Tauri expects a fixed port, fail if that port is not available
     strictPort: true,
+    // if the host Tauri is expecting is set, use it
     host: host || false,
-    hmr: host
-      ? {
-          protocol: 'ws',
-          host,
-          port: 1421,
-        }
-      : undefined,
-    watch: {
-      // 3. tell vite to ignore watching `src-tauri`
-      ignored: ['**/src-tauri/**'],
+    port: 5173,
+  },
+
+  // Env variables starting with the item of `envPrefix` will be exposed in tauri's source code through `import.meta.env`.
+  envPrefix: ['VITE_', 'TAURI_ENV_*'],
+  build: {
+    // Tauri uses Chromium on Windows and WebKit on macOS and Linux
+    target: process.env.TAURI_ENV_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
+    // don't minify for debug builds
+    minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' : false,
+    // produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_ENV_DEBUG,
+
+    rollupOptions: {
+      onwarn: quietUseClientDirective,
     },
   },
+
   optimizeDeps: {
     exclude: ['@swc/wasm-web'],
   },
@@ -40,11 +44,6 @@ export default defineConfig(async () => ({
   resolve: {
     alias: {
       ...(await workspaceAliases()),
-    },
-  },
-  build: {
-    rollupOptions: {
-      onwarn: quietUseClientDirective,
     },
   },
 }))
