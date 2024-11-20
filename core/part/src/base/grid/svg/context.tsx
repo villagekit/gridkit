e26087e7df75afecee855f43f07ced5a934c84bb
Michael Williams <hello@mikey.nz>
2024-11-20 17:09:26 +1300

    Use constant font size in responsive svg viewbox

--- core/part/src/base/grid/svg/context.tsx
+++ core/part/src/base/grid/svg/context.tsx
@@ -1,16 +1,32 @@
+import { Box } from '@villagekit/ui'
 import { createContext, useContext } from 'react'
+import useMeasure from 'react-use-measure'
 
 interface GridSvgContextProps {
   displayUnit: 'gu' | 'mm'
 }
 
-interface GridSvgContextType extends GridSvgContextProps {}
+interface GridSvgContextType extends GridSvgContextProps {
+  viewWidth: number
+}
 
 const GridSvgContext = createContext<GridSvgContextType | null>(null)
 
 export function GridSvgContextProvider(props: React.PropsWithChildren<GridSvgContextProps>) {
-  const { children, ...value } = props
-  return <GridSvgContext.Provider value={value}>{children}</GridSvgContext.Provider>
+  const { children, ...contextProps } = props
+
+  const [ref, bounds] = useMeasure()
+  const viewWidth = bounds.width
+
+  const value = { ...contextProps, viewWidth }
+
+  return (
+    <GridSvgContext.Provider value={value}>
+      <Box sx={{ width: '100%' }} ref={ref}>
+        {children}
+      </Box>
+    </GridSvgContext.Provider>
+  )
 }
 
 export function useGridSvgContext(): GridSvgContextType {
